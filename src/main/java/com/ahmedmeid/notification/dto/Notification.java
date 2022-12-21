91ed8e4a01d45a74e1b27447dd74395cdbc0b0c5
Ahmed Mohamed <ahmed.eid@gmx.com>
2022-12-21 19:09:31 +0300

    Initial commit

---
+++ src/main/java/com/ahmedmeid/notification/dto/Notification.java
@@ -0,0 +1,120 @@
+package com.ahmedmeid.notification.dto;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "subscriptionId",
+    "data"
+})
+@Generated("jsonschema2pojo")
+public class Notification {
+
+    @JsonProperty("subscriptionId")
+    private String subscriptionId;
+    @JsonProperty("data")
+    private List<Datum> data = new ArrayList<Datum>();
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("subscriptionId")
+    public String getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    @JsonProperty("subscriptionId")
+    public void setSubscriptionId(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    public Notification withSubscriptionId(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+        return this;
+    }
+
+    @JsonProperty("data")
+    public List<Datum> getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+    public Notification withData(List<Datum> data) {
+        this.data = data;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public Notification withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Notification.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("subscriptionId");
+        sb.append('=');
+        sb.append(((this.subscriptionId == null)?"<null>":this.subscriptionId));
+        sb.append(',');
+        sb.append("data");
+        sb.append('=');
+        sb.append(((this.data == null)?"<null>":this.data));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        result = ((result* 31)+((this.subscriptionId == null)? 0 :this.subscriptionId.hashCode()));
+        result = ((result* 31)+((this.data == null)? 0 :this.data.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Notification) == false) {
+            return false;
+        }
+        Notification rhs = ((Notification) other);
+        return ((((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties)))&&((this.subscriptionId == rhs.subscriptionId)||((this.subscriptionId!= null)&&this.subscriptionId.equals(rhs.subscriptionId))))&&((this.data == rhs.data)||((this.data!= null)&&this.data.equals(rhs.data))));
+    }
+
+}
